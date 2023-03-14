7d6059b4bf8f6e46e42d897da30954aab786b5fb
guangmeiye <47375532+guangmeiye@users.noreply.github.com>
2023-03-13 23:21:38 -0400

    add openai backend base code

--- tarot_drawing/models.py
+++ tarot_drawing/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class ChatGPTConnection(models.Model):
+    API_KEY_CHOICES = [
+        ('davinci', 'Davinci'),
+        ('curie', 'Curie'),
+        ('babbage', 'Babbage'),
+        ('ada', 'Ada'),
+    ]
+
+    name = models.CharField(max_length=255)
+    api_key = models.CharField(max_length=255, choices=API_KEY_CHOICES)
+    max_tokens = models.PositiveIntegerField(default=1024)
+    temperature = models.DecimalField(max_digits=4, decimal_places=2, default=0.7)
+    frequency_penalty = models.DecimalField(max_digits=4, decimal_places=2, default=0)
+    presence_penalty = models.DecimalField(max_digits=4, decimal_places=2, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "ChatGPT Connections"
+
+    def __str__(self):
+        return self.name
--- tarot_drawing/views.py
+++ tarot_drawing/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
+import os
+import openai
 
-# Create your views here.
+
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+
 def tarot_drawing(request):
-    return render(request, 'tarot_drawing.html', {})
+    if request.method == 'POST':
+        # Get the key sentence from the request POST data
+        key_sentence = request.POST.get('key_sentence', '')
+
+        # Generate a response using the ChatGPT API
+        response = openai.Completion.create(
+            engine='davinci',
+            prompt=key_sentence,
+            max_tokens=1024,
+            temperature=0.7,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+
+        # Extract the generated text from the response
+        answer = response.choices[0].text
+
+        # Render the template with the generated answer
+        return render(request, 'tarot_drawing.html', {'answer': answer})
+
+    else:
+        # Render the template with an empty form
+        return render(request, 'tarot_drawing.html', {})
